7722c44b7faa182d7f39d6aaf7bb77ee48af4311
Ilya Lakhin <eliah.lakhin@gmail.com>
2013-10-30 22:08:40 +0700

    Engine configuration intermediate result

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,56 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+
+        jade: {
+            template: {
+                options: {
+                    pretty: true
+                },
+                files: {
+                    'www/index.html': 'source/template/index.jade'
+                }
+            }
+        },
+
+        connect: {
+            preview: {
+                options: {base: 'www'}
+            }
+        },
+
+        watch: {
+            jade: {
+                files: ['source/template/**/*.jade', 'source/article/**/*.md'],
+                expand: true,
+                tasks: ['jade:template'],
+                options: {spawn: false}
+            }
+        },
+
+        markdown: {
+            article: {
+                cwd: 'source/article',
+                src: ['**/*.md'],
+                expand: true,
+                dest: 'www/html/',
+                ext: '.html',
+                options: {
+                    template: 'source/article/template.txt',
+                    markdownOptions: {
+
+                    }
+                }
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('compile', ['jade:template']);
+    grunt.registerTask('default', ['compile', 'connect:preview', 'watch']);
 };
